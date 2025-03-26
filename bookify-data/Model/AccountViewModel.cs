b5ab1d20b361660caa2f66e9901bd00806b848a4
duyng21303 <duyng21303@gmail.com>
2025-03-26 15:11:55 +0700

    a

---
+++ bookify-data/Model/AccountViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bookify_data.Model
+{
+    public class AccountViewModel
+    {
+        public int AccountId { get; set; }
+        public string? Username { get; set; }
+        public string? DisplayName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public int RoleId { get; set; }
+
+        // Nếu muốn lấy tên role, bạn có thể thêm:
+        public string? RoleName { get; set; }
+
+        // Lấy danh sách Note (nếu cần chi tiết)
+        public List<NoteViewModel> Notes { get; set; } = new List<NoteViewModel>();
+
+    }
+}
